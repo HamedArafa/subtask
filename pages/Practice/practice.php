0f8e133e0772ca9681b65c1e68b6202420703d23
hamedarafa <hamed.arafa.h@gmail.com>
2014-12-30 11:42:10 +0200

    nothing important

---
+++ pages/Practice/practice.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<script type='text/javascript' src='../../bower_components/jquery/dist/jquery.js'></script>		
+		<script type='text/javascript' src='../../bower_components/bootstrap/dist/js/bootstrap.js'></script>
+		<link rel='stylesheet' href='../../bower_components/bootstrap/dist/css/bootstrap.min.css'>
+		<link rel='stylesheet' href='../../globalstyling.css'>
+
+	</head>
+	<body>
+		<div class= "problem-discription">
+
+		</div>
+		
+		<div class="form-group">
+			<textarea class="form-control subtask-input-code" rows="5" id="comment"></textarea>
+		</div>
+		<input type="button" class="btn btn-primary" value="submit" name="submit_btn" id="submit_btn" />
+
+	</body>
+</html>
